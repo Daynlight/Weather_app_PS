2ea8e6229c26fc39362b33b2702d4660b5605adf
Daynlight <danielstodulski000@gmail.com>
2026-03-24 12:46:10 +0100

    JsonParser get json String

--- App/src/main/java/org/example/JsonParser.java
+++ App/src/main/java/org/example/JsonParser.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.example.WeatherAppAPI;
 import org.example.Terminal;
 
 import java.util.List;
@@ -13,22 +12,23 @@ import com.fasterxml.jackson.core.type.TypeReference;
 
 
 class JsonParser{
-  private WeatherAppAPI api = new WeatherAppAPI();
   private Terminal terminal = new Terminal();
   private static List<Root> roots = null;
+  String json_ref = null;
   
 
-  public List<Root> getRootsFromApi(){
-    if(roots != null) {
+  public List<Root> getRootsFromApi(String json){
+    if(roots != null && json_ref == json ) {
       terminal.printLog("Used cached roots");
       return roots;
     };
 
     try {
       ObjectMapper mapper = new ObjectMapper();
-      roots =  mapper.readValue(api.get(), new TypeReference<List<Root>>() {});
+      roots =  mapper.readValue(json, new TypeReference<List<Root>>() {});
       terminal.printLog("Parsing json");
       terminal.printLog("Roots are cached");
+      json_ref = json;
     } catch (Exception e) {
       terminal.printError("Parsing Json Error: " + e);
       e.printStackTrace();
--- App/src/main/java/org/example/Statistics.java
+++ App/src/main/java/org/example/Statistics.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import org.example.WeatherAppAPI;
 import org.example.Terminal;
 import org.example.JsonParser;
 
@@ -14,11 +15,13 @@ import org.fusesource.jansi.Ansi.Color;
 class Statistics{
   private JsonParser json = new JsonParser();
   private Terminal terminal = new Terminal();
+  private WeatherAppAPI api = new WeatherAppAPI();
 
 
   public void print(){
     terminal.printLog("Printing Statistics");
-    json.getRootsFromApi();
+    json.getRootsFromApi(api.get());
+    json.getRootsFromApi(api.get());
 
     // for (Root root : json.getRootsFromApi()) {
     //   if (root.getLocation() != null) {
