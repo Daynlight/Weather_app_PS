bb75d77aace9806843ab01fa1a54ecc763d58b05
Daynlight <danielstodulski000@gmail.com>
2026-03-24 13:02:22 +0100

    avg Temp and hightPM Stats

--- App/src/main/java/org/example/Statistics.java
+++ App/src/main/java/org/example/Statistics.java
@@ -20,14 +20,33 @@ class Statistics{
 
   public void print(){
     terminal.printLog("Printing Statistics");
-    json.getRootsFromApi(api.get());
-    json.getRootsFromApi(api.get());
+    
+    terminal.print("Average Temperature: " + avgTemp());
+    terminal.print("High PM 25: " + highPM(25));
+  };
 
-    // for (Root root : json.getRootsFromApi()) {
-    //   if (root.getLocation() != null) {
-    //     terminal.print(root.getLocation().getCountry());
-    //   };
-    // };
+  private double avgTemp(){
+    double avgTemp = json.getRootsFromApi(api.get()).stream()
+      .map(Root::getSensordatavalues)
+      .flatMap(List::stream)
+      .filter(sd -> sd.getValue_type().equals("temperature"))
+      .mapToDouble(sd -> Double.parseDouble(sd.getValue()))
+      .average()
+      .orElse(0.0);
+    
+    return avgTemp;
+  };
+
+  private long highPM(double value){
+    long highPmCount = json.getRootsFromApi(api.get()).stream()
+      .map(Root::getSensordatavalues)
+      .flatMap(List::stream)
+      .filter(sd -> sd.getValue_type().equals("temperature") && 
+        sd.getValue().matches("-?\\d+(\\.\\d+)?") && 
+        Double.parseDouble(sd.getValue()) > value)
+      .count();
+      
+    return highPmCount;
   };
 
 };
